598c00fc9c184ac6ed31c729e31d76f3fbcb83af
Viktor Bahr <viktor@bccn-berlin.de>
2017-11-29 16:03:59 +0100

    Updated drop table file. Closing Issue #2.

--- sql/drop_table.m
+++ sql/drop_table.m
@@ -1,5 +1,27 @@
 function sql_drop(DB)
+%drop_table Simple wrapper for mysql drop table routine.
+%
+%   Syntax: drop_table(DB)
+%
+%   [IN]
+%       DB       :  Struct with table names as fieldnames.
+%
+% Copyright (C) 2017  Viktor Bahr (viktor [at] bccn-berlin.de)
+% 
+% This program is free software: you can redistribute it and/or modify
+% it under the terms of the GNU General Public License as published by
+% the Free Software Foundation, either version 3 of the License, or
+% (at your option) any later version.
+% 
+% This program is distributed in the hope that it will be useful,
+% but WITHOUT ANY WARRANTY; without even the implied warranty of
+% MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+% GNU General Public License for more details.
+% 
+% You should have received a copy of the GNU General Public License
+% along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
+% test sql connection
 try
     c = evalc('d = mysql(''show tables'')');
 catch me
@@ -24,8 +46,12 @@ if isstruct(DB)
         fprintf('Droppped ''%s'' table.\n', char(t));
     end
 else
-    c = evalc('mysql(sprintf(''drop table %s'', DB));');
-    fprintf('Droppped ''%s'' table.\n', DB);
+    try
+        c = evalc('mysql(sprintf(''drop table %s'', DB));');
+        fprintf('Droppped ''%s'' table.\n', DB);
+    catch me
+        fprintf('Unable to drop ''%s''.\n', char(t));
+    end
 end
 
 end
