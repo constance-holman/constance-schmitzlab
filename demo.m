2f3decd9c3b5e64daedf8fcca8c25dc957c7e7d9
Viktor Bahr <viktor@bccn-berlin.de>
2017-12-21 16:17:01 +0100

    Updated demo script

--- demo.m
+++ demo.m
@@ -1,12 +1,18 @@
+%% connection
+
 % connect to database
 mysql('open','mysql','viktor','viktor');
 mysql('use','viktor');
+
+%% setup
 
 % add sql folder to path
 addpath('sql/');
 
 % init create queries
 run('init.m');
+
+%% create / drop tables
 
 % show tables
 mysql('show tables');
@@ -16,3 +22,9 @@ drop_table(db);
 
 % create tables
 create_table(db);
+
+%% insert data
+
+% insert project
+project_id = insert_project('Test');
+
