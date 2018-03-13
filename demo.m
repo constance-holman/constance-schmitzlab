fbf6adb1e80fe9687df827b7adb9f26608019b88
Viktor Bahr <viktor@bccn-berlin.de>
2018-03-13 17:59:54 +0100

    Added all insertion methods to demo. Added some real-life meta-data to insert.

--- demo.m
+++ demo.m
@@ -26,7 +26,7 @@ drop_table(db);
 % create tables
 create_table(db);
 
-%% insert data
+%% insert toy data
 
 % insert project
 project_id = insert_project('Test Project');
@@ -61,14 +61,27 @@ reward_type_id = insert_rewardtype('Sugar', 'positive');
 % insert reward
 insert_reward(session_id, reward_type_id, [1;100;1000]);
 
+%% insert real metadata
+
 % insert amplifier
-amplifier_id = insert_amplifier('Test Amp');
+amp.Amplipex = insert_amplifier('Amplipex');
+amp.Intan = insert_amplifier('Intan');
 
 % insert probe type
-probe_type_id = insert_probetype('Test Probe type');
+probe.NN4x8 = insert_probetype('NN32_4x8LFP');
+probe.NN2x8 = insert_probetype('NN32_2x8LFP');
+probe.CNT32 = insert_probetype('CNT32_Edge2x16');
+probe.NNpoly2 = insert_probetype('NN32_poly2');
+probe.NNpoly3 = insert_probetype('NN32_poly3');
+probe.NN32edge = insert_probetype('NN32_Edge1x32');
+probe.NN32poly2opto = insert_probetype('NN32_poly2optrode');
+probe.NN32poly3opto = insert_probetype('NN32_poly3optrode');
+probe.NN32ISO3 = insert_probetype('NN32_ISO3xtet');
+
+%%
 
 % insert probe
-probe_id = insert_probe(probe_type_id, 'ABCDEFGHIJKLMNOPQRST');
+probe_id = insert_probe(probe.NN4x8, 'ABCDEFGHIJKLMNOPQRST');
 
 % insert shank
 shank_id = insert_shank(probe_id, 'Sites', 20);
@@ -77,17 +90,30 @@ shank_id = insert_shank(probe_id, 'Sites', 20);
 insert_sitepos(shank_id, [1,1;2,1;3,1], [1;2;3]);
 
 % insert remapping
-insert_remapping(probe_type_id, amplifier_id, ...
+insert_remapping(probe.NN4x8, amp.Amplipex, ...
     'Probe', [1;2;3], 'Headstage', [4;7;9]);
 
 % insert recording
-rec_id = insert_recording(session_id, probe_id, amplifier_id, 100, ...
+rec_id = insert_recording(session_id, probe_id, amp.Amplipex, 100, ...
     'Note', 'Not deep enough...');
 
 % insert histology
 histology_id = insert_histology(rec_id, 'Fuchsia', 'COX');
 
 % insert anatomy
-insert_anatomy(histology_id, [0.7; 1.0], {'vhc'; 'fi'}, [1; 2])
+insert_anatomy(histology_id, {'vhc'; 'fi'}, [1; 2])
 
+% insert drug injection
+insert_druginjection(rec_id, 'Lidocain', [1;100;1000]);
+
+% insert electro stimulation
+insert_electrostim(rec_id, [100;200;300], [0.5;0.3;0.5]);
+
+% insert optogenetic stimulation
+insert_optostim(rec_id, [100;200;300], [650;650;650]);
+
+% insert ephys data
+insert_lfp(rec_id, [1;1;1], [100;200;300], [0.3;0.4;0.5]);
+insert_juxta(rec_id, 'rec', [100;200;300], [0.3;0.4;0.5]);
+insert_patch(rec_id, 'rec', [100;200;300], [0.3;0.4;0.5])
 
